b4694e597410b9046986b027b41436898596e307
seung.bok <seungbok.lee@huinnoaim.com>
2023-11-01 17:59:24 +0900

    get<type>은 의미가 없다

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,8 +10,8 @@ export class AppController {
     private readonly appService: AppService,
     private readonly configService: ConfigService
   ) {
-    this.null_check = configService.getOrThrow('NULL_CHECK')
-    this.not_null_check = configService.getOrThrow('NOT_NULL_CHECK')
+    // this.null_check = configService.getOrThrow('NULL_CHECK')
+    // this.not_null_check = configService.getOrThrow('NOT_NULL_CHECK')
   }
 
   @Get()
@@ -70,7 +70,18 @@ export class AppController {
 
   @Get('db4')
   envTest2(): any {
-    console.info('this.not_null_check', this.not_null_check)
+    // get<number>는 숫자로 변환하지 않는다
+    const n1 = this.configService.get<number>('INT_TEST1')
+    console.info('n1', n1 + 100)
+    const n2 = this.configService.get<number>('INT_TEST2')
+    console.info('n2', n2 + 100)
+    const n3 = this.configService.get<number>('INT_TEST3')
+    console.info('n3', n3 + 100)
+    const n4 = parseInt(this.configService.get<string>('INT_TEST4'))
+    console.info('n4', n4 + 100)
+
+    const t = n1 + 300
+    console.info('t', t)
   }
 }
 
